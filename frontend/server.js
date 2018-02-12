053fbce35f560e3c951f38109e2d9a995e260d08
Jardine <jardine.miller@gmail.com>
2018-02-12 10:18:33 +0000

    commented out unused code

--- frontend/server.js
+++ frontend/server.js
@@ -6,24 +6,23 @@ server.use(parser.json());
 server.use(parser.urlencoded({extended: true}));
 server.use(express.static('build'));
 
-server.get('/portfolio/:name', function(req, res) {
-  res.sendFile(__dirname + '/build/index.html');
-  // const options = {
-  //   url: 'http://localhost:9000/api/portfolio/' + req.params.name,
-  //   method: 'GET',
-  //   headers: {
-  //       'Accept': 'application/json',
-  //       'Accept-Charset': 'utf-8',
-  //   }
-  // };
-  // request(options, function(error, response, body) {
-  //   let json = JSON.parse(body);
-  //   console.log(json);
-  //   console.log(response);
-  //   console.log(body);
-  //   res.json(json);
-  // });
-})
+// server.get('/portfolio/:name', function(req, res) {
+//   const options = {
+//     url: 'http://localhost:9000/api/portfolio/' + req.params.name,
+//     method: 'GET',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Accept-Charset': 'utf-8',
+//     }
+//   };
+//   request(options, function(error, response, body) {
+//     let json = JSON.parse(body);
+//     console.log(json);
+//     console.log(response);
+//     console.log(body);
+//     res.json(json);
+//   });
+// })
 
 server.get('/api/:coin', function(req, res) {
   const options = {
